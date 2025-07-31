c1f94700fb2fedbfc3df42fb18bccf3849f63946
Fouzia-Oreen <fouziaoreen236@gmail.com>
2025-08-01 00:16:29 +0600

    updated

--- server/server.js
+++ server/server.js
@@ -8,40 +8,32 @@ import { auth } from './middlewares/auth.js';
 import aiRouter from './routes/aiRoutes.js';
 import userRouter from './routes/userRoutes.js';
 
-const PORT = 3090;
+
 const app = express();
 
-const startServer = async () => {
-  await connectCloudinary();
-  await connectDb(); 
 
-  app.use(cors());
+connectCloudinary();
+connectDb(); 
 
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(clerkMiddleware());
 
-  app.use(clerkMiddleware());
-
-  //  Public route
-  app.get('/', (req, res) => {
+// Public route
+app.get('/', (req, res) => {
     res.send('Welcome to the Genora Hub AI Server!');
-  });
+});
 
-  //  Protected routes
-  app.use('/api/ai', requireAuth(), auth, aiRouter);
-  app.use('/api/user', requireAuth(), auth, userRouter);
+// Protected routes
+app.use('/api/ai', requireAuth(), auth, aiRouter);
+app.use('/api/user', requireAuth(), auth, userRouter);
 
-  // Error handler
-  app.use((err, req, res, next) => {
+// Error handler
+app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
-  });
-
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-};
-
-startServer().catch(err => {
-  console.error('Failed to start server:', err);
 });
+
+
+export default app;
